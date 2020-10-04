289a1b1dcde68382407e3fa1777b42279c0f5b1a
Taras Vlasiuk <tarasvlasiuk0@gmail.com>
2020-10-04 23:50:50 +0300

    work hw-08

--- js/modal.js
+++ js/modal.js
@@ -10,6 +10,8 @@ refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
-refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('modal-open');
+    
+refs.modal.classList.toggle("is-hidden");
 }
 })();
